39ab9186be715bf64709c9e2465ac4b7a6122906
Elias Johansson <elias_johansson@hotmail.se>
2018-06-17 23:45:52 +0200

    Replace mentions and urls with links

--- client/src/containers/shared/Tweet/Content.js
+++ client/src/containers/shared/Tweet/Content.js
@@ -4,11 +4,26 @@ import styled from 'styled-components';
 import reactStringReplace from 'react-string-replace';
 import { NavLink } from 'react-router-dom';
 
+const replaceEntityStrings = (text) => {
+  let replacedText;
+  // Replace Hashtags
+  replacedText = reactStringReplace(text, /#(\w+)/g, (match, i) => (
+    <Entity key={`hashtag-${i}`} to={`/hashtag/${match}`}>#<span>{match}</span></Entity>
+  ));
+  // Replace and shorten URL's
+  replacedText = reactStringReplace(replacedText, /(https?:\/\/\S+)/g, (match, i) => (
+    <URL key={`url-${i}`} href={match}><span>{match.substring(0, 22)}...</span></URL>
+  ));
+  // Replace Mentions
+  replacedText = reactStringReplace(replacedText, /@(\w+)/g, (match, i) => (
+    <Entity key={`mention-${i}`} to={`/${match}`}>@<span>{match}</span></Entity>
+  ));
+  return replacedText;
+};
+
 const Content = ({ data }) => (
   <StyledContent>
-    {reactStringReplace(data.text, /#(\w+)/g, (match, i) => (
-      <Hashtag key={`hashtag-${i}`} to={`/hashtag/${match}`}>#<span>{match}</span></Hashtag>
-    ))}
+    { replaceEntityStrings(data.text) }
   </StyledContent>
 );
 
@@ -18,12 +33,14 @@ Content.propTypes = {
   }).isRequired,
 };
 
+
+// Style
 const StyledContent = styled.div`
   font-size: 14px;
   line-height: 20px;
 `;
 
-const Hashtag = styled(NavLink)`
+const Entity = styled(NavLink)`
   color: #1B95E0;
   font-weight: normal;
   text-decoration: none;
@@ -34,5 +51,7 @@ const Hashtag = styled(NavLink)`
     }
   }
 `;
+
+const URL = Entity.withComponent('a');
 
 export default Content;
--- client/src/containers/shared/Tweet/Tweet.stories.js
+++ client/src/containers/shared/Tweet/Tweet.stories.js
@@ -6,32 +6,17 @@ import Tweet from './';
 // Dummy data
 const baseTweet = {
   text: 'This is a test tweet.',
-  user: {
-    screenName: 'Elias Johansson',
-    username: 'Geggs',
-  },
-  stats: {
-    replyCount: 0,
-    retweetCount: 0,
-    favoriteCount: 0,
-  },
-  createdAt: '2018-06-13 23:19:35.183',
-  updatedAt: '2018-06-13 23:19:35.183',
+  user: { screenName: 'Elias Johansson', username: 'Geggs' },
+  stats: { replyCount: 0, retweetCount: 0, favoriteCount: 0 },
 };
 
-const withHastag = {
-  ...baseTweet,
-  text: 'This is a test tweet. #hashtag',
-};
-
-const withStats = {
-  ...baseTweet,
-  stats: {
-    replyCount: 51,
-    retweetCount: 23,
-    favoriteCount: 103,
-  },
-};
+const tweets = [
+  { ...baseTweet },
+  { ...baseTweet, text: 'This is a test tweet. #hashtag' },
+  { ...baseTweet, text: 'This is a test tweet. @mention' },
+  { ...baseTweet, text: 'This is a test tweet. http://mycoolwebsite.com/some-nice-route' },
+  { ...baseTweet, stats: { replyCount: 51, retweetCount: 23, favoriteCount: 103 } },
+];
 
 const Decorator = styled.div`
   @import 'https://use.fontawesome.com/releases/v5.0.13/css/all.css';
@@ -43,6 +28,8 @@ const Decorator = styled.div`
 
 storiesOf('Tweet', module)
   .addDecorator(story => <Decorator>{story()}</Decorator>)
-  .add('only text', () => <Tweet data={baseTweet} />)
-  .add('with hashtag', () => <Tweet data={withHastag} />)
-  .add('with stats', () => <Tweet data={withStats} />);
+  .add('base (only text)', () => <Tweet data={tweets[0]} />)
+  .add('with hashtag', () => <Tweet data={tweets[1]} />)
+  .add('with mention', () => <Tweet data={tweets[2]} />)
+  .add('with url', () => <Tweet data={tweets[3]} />)
+  .add('with stats', () => <Tweet data={tweets[4]} />);
