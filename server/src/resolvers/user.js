9f8f0807e6e529d9d6adfa2f96fc774dabb0878e
Elias Johansson <elias_johansson@hotmail.se>
2018-09-13 12:12:35 +0200

    User follow relations

--- server/src/resolvers/user.js
+++ server/src/resolvers/user.js
@@ -4,7 +4,18 @@ const authRequired = isAuthenticated.createResolver;
 
 // Queries
 const getUsers = authRequired((parent, args, { models }) => models.User.find());
-const getUser = authRequired((parent, args, { models }) => models.User.findOne(args));
+const getUser = authRequired(async (parent, args, { models, user: authedUser }) => {
+  const user = await models.User.findOne(args);
+  const following = await models.Follow.count({ follower: authedUser, followee: user._id });
+  const followingMe = await models.Follow.count({ follower: user._id, followee: authedUser });
+
+  user.relation = {
+    following: following >= 1,
+    followingMe: followingMe >= 1,
+  };
+
+  return user;
+});
 
 // Mutations
 const updateUser = authRequired(async (parent, args, { models, user }) => {
